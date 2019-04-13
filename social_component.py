70284adbd1c6aa21e24f00ba0db9b8a380495f9c
Karan Newatia <kn348@cornell.edu>
2019-04-13 18:54:22 -0400

    reddit api call

--- social_component.py
+++ social_component.py
@@ -10,11 +10,14 @@ with open("./data/name_id_lookup.json", "r") as f:
 
     for key in name_ids:
         new_key = key.replace("'","%27")
-        url = "https://api.pushshift.io/reddit/search/comment/?q=" + new_key + "&subreddit=travel&fields=score,body&sort_type=score&size=50"
-        api = requests.get(url)
+        new_key = urllib.parse.urlencode({'q':key})
+
+        url = "https://api.pushshift.io/reddit/search/comment/?&subreddit=travel&fields=score,body&sort_type=score&size=50"
+        api = requests.get(url, new_key)
         api_call = api.content.decode('utf8').replace("''", '""')
         api_call = ast.literal_eval(api_call)
         results[key] = api_call["data"]
+        break
 
 with open("./data/reddit.json", "w") as f:
      json.dump(results, f)
