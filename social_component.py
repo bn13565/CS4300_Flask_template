ebbc7264256f0107c3efa33099c3400d1b99e7fe
Karan Newatia <kn348@cornell.edu>
2019-04-14 19:06:38 -0400

    new jsons

--- social_component.py
+++ social_component.py
@@ -94,42 +94,69 @@ import numpy as np
 #     if key in dict_new:
 #         inlinks[key] += 0.25
 
-with open ('./data/tf.pickle', 'rb') as f:
-    tf_transcripts = pickle.load(f)
-    tf_array = tf_transcripts.toarray()
+# with open ('./data/tf.pickle', 'rb') as f:
+#     tf_transcripts = pickle.load(f)
+#     tf_array = tf_transcripts.toarray()
+#
+# with open ('./data/inverted_index.json') as wil_file:
+#    inverted_index = json.load(wil_file)
+#
+# with open ('./data/word_id_lookup.json') as wil_file:
+#     word_id_lookup = json.load(wil_file)
+#
+# with open ('./data/name_id_lookup.json') as wil_file:
+#     name_id_lookup = json.load(wil_file)
+#
+# with open ('./data/idf.json') as wil_file:
+#    idf = json.load(wil_file)
+#
+# with open ('./data/inverted_dict_id_word.json') as wil_file:
+#     inverted_dict_id_word = json.load(wil_file)
+#
+# with open ('./data/inverted_dict_id_name.json') as wil_file:
+#     inverted_dict_id_name = json.load(wil_file)
 
-with open ('./data/inverted_index.json') as wil_file:
-   inverted_index = json.load(wil_file)
+# with open ('./data/preprocessed_wikivoyage_notext.json') as wil_file:
+#     wiki = json.load(wil_file)
+#
+# display_names = {}
+#
+# for key in wiki:
+#     display_name = wiki[key]["display_title"]
+#     url = "https://en.wikivoyage.org/wiki/" + urllib.parse.quote(display_name)
+#     display_names[key] = (display_name, url)
+#
+# with open("./data/display_name_url.json", "w") as f:
+#       json.dump(display_names, f)
 
-with open ('./data/word_id_lookup.json') as wil_file:
-    word_id_lookup = json.load(wil_file)
+reddit_shortened = {}
 
-with open ('./data/name_id_lookup.json') as wil_file:
-    name_id_lookup = json.load(wil_file)
+with open ('./data/reddit.json') as wil_file:
+     reviews = json.load(wil_file)
 
-with open ('./data/idf.json') as wil_file:
-   idf = json.load(wil_file)
+for key in reviews:
+    if len(reviews[key]) > 5:
+        reddit_shortened[key] = reviews[key][:5]
+    else:
+        reddit_shortened[key] = reviews[key]
 
-with open ('./data/inverted_dict_id_word.json') as wil_file:
-    inverted_dict_id_word = json.load(wil_file)
+#urllib.parse.quote("bora bora")
 
-with open ('./data/inverted_dict_id_name.json') as wil_file:
-    inverted_dict_id_name = json.load(wil_file)
-
-doc_norms = np.zeros(len(tf_array))
-doc_norms_dict = {}
-
-for key in idf:
-    key_name = inverted_dict_id_word[key]
-    for idx,count in inverted_index[key_name]:
-        score = np.square(count * idf[key])
-        doc_norms[idx] += score
-
-for i in range(len(doc_norms)):
-    doc_norms_dict[i] = doc_norms[i]
-
-#print(doc_norms_dict)
-
-
-with open("./data/doc_norms.json", "w") as f:
-      json.dump(doc_norms_dict, f)
+#
+# doc_norms = np.zeros(len(tf_array))
+# doc_norms_dict = {}
+#
+# for key in idf:
+#     key_name = inverted_dict_id_word[key]
+#     for idx,count in inverted_index[key_name]:
+#         score = np.square(count * idf[key])
+#         doc_norms[idx] += score
+#
+# for i in range(len(doc_norms)):
+#     doc_norms_dict[i] = doc_norms[i]
+#
+# #print(doc_norms_dict)
+#
+#
+with open("./data/reddit_shortened.json", "w") as f:
+      json.dump(reddit_shortened, f)
