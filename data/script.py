df55324d769c9100472a8ac06bfb34f0dcd53f0a
Robert Yang <ry92@cornell.edu>
2019-04-15 23:05:11 -0400

    Increasing new inverted index size

--- data/script.py
+++ data/script.py
@@ -3,5 +3,13 @@ import json
 with open('inverted_index.json') as wil_file:
     data = json.load(wil_file)
 
-with open('new_inverted_index.json', 'w') as wil_file:
-    json.dump(data["southward"], wil_file)
+results = {}
+counter = 0
+for key in data:
+  results[key] = data[key]
+  counter += 1
+  if counter == 1000:
+    break
+
+with open('new_inverted_index.json', 'w') as wil_file:\
+    json.dump(results, wil_file)
